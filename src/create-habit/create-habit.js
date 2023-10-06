2397c3629383b31a124cc2f9ba654bbef9a5768b
YeethanBoi <yeethan007@gmail.com>
2023-10-06 16:46:59 +1030

    updated the create habit page

--- src/create-habit/create-habit.js
+++ src/create-habit/create-habit.js
@@ -1,97 +1,151 @@
-// import the appropriate CSS files
-// main.css contains the css for the entire app, including tailwin
-// import modules that are needed
-// import { db } from "../utils/firebase-utils";
-
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-import {
-  getFirestore,
-  collection,
-  doc,
-  addDoc,
-  setDoc,
-  getDoc,
-  updateDoc,
-  getDocs,
-  query,
-  where,
-  serverTimestamp,
-  orderBy,
-  limit,
-  onSnapshot,
-  deleteDoc,
-} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
-import {
-  getAuth,
-  signInWithRedirect,
-  GoogleAuthProvider,
-  onAuthStateChanged,
-  getRedirectResult,
-  signOut,
-} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
-const firebaseConfig = {
-  apiKey: "AIzaSyBPUq_3dMVUYPA94SBHBAtkAWsFlySNs6k",
-  authDomain: "hablits-8a957.firebaseapp.com",
-  projectId: "hablits-8a957",
-  storageBucket: "hablits-8a957.appspot.com",
-  messagingSenderId: "607760813629",
-  appId: "1:607760813629:web:34dd984572201352c4a454",
-};
-
 import "../styles/main.css";
 const loader = document.getElementById("loader");
 loader.style.display = "none";
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
+const pictureSelect = document.getElementById("picture-selector");
 
-let fileInput = document.getElementById("image-upload");
+const presetSelect = document.getElementById("preset-selector");
 
-fileInput.addEventListener("change", uploadFile(), false);
+const habitPicture = document.getElementById("habit-picture");
 
-async function uploadFile() {
-  console.log("Working");
-  var file = fileInput.files[0];
+pictureSelect.addEventListener("change", changedSelectedPicture, false);
 
-  // Create a reference to the file in Firestore storage
-  var storageRef = firebase.storage().ref().child(file.name);
+presetSelect.addEventListener("change", changePresets, false);
 
-  // Upload file to Firestore storage
-  storageRef
-    .put(file)
-    .then(function (snapshot) {
-      console.log("File uploaded successfully!");
-
-      // Get the download URL of the uploaded file
-      snapshot.ref
-        .getDownloadURL()
-        .then(function (downloadURL) {
-          // Save the download URL to Firestore database
-          db.collection("test")
-            .doc(file.name)
-            .set({
-              name: file.name,
-              url: downloadURL,
-            })
-            .then(function () {
-              console.log("Download URL saved to Firestore database.");
-
-              // Display the uploaded image
-              let uploadedImage = document.getElementById("uploadedImage");
-              uploadedImage.src = downloadURL;
-            })
-            .catch(function (error) {
-              console.error("Error saving download URL to Firestore:", error);
-            });
-        })
-        .catch(function (error) {
-          console.error("Error getting download URL:", error);
-        });
-    })
-    .catch(function (error) {
-      console.error("Error uploading file:", error);
-    });
+function changedSelectedPicture() {
+  console.log("Registered change");
+  if (pictureSelect.value == "none") {
+    console.log("Chose Default Option");
+    habitPicture.src = "../assets/allhabits.png";
+  } else {
+    habitPicture.src = `../assets/${pictureSelect.value}.png`;
+  }
 }
+
+function changePresets() {
+  let selectedPreset;
+  for (let i = 0; i < presets.length; i++) {
+    console.log(presetSelect.value + "||" + presets[i].name);
+    if (presetSelect.value == presets[i].name) {
+      console.log("The preset chosen was " + presetSelect.value);
+      selectedPreset = presets[i];
+    }
+  }
+
+  console.log(selectedPreset);
+}
+
+habitPicture.src = "../assets/allhabits.png";
+
+const guitar = {
+  isDaily: true,
+  name: "guitar",
+  cue: "When I see my guitar by the corner of the room",
+  craving: "I want to improve my guitar skills and enjoy playing music",
+  action: "I will practice guitar for 30 minutes",
+  reward:
+    "I will feel a sense of accomplishment and closer to my musical goals",
+  goal: "30 minutes",
+  percentageImprovement: 5, // 5% improvement
+};
+
+const meditation = {
+  isDaily: true,
+  name: "meditation",
+  cue: "When I sit on my meditation cushion in the morning",
+  craving: "I want to find inner peace and reduce stress",
+  action: "I will meditate for 15 minutes",
+  reward: "I will experience a calm and focused mind",
+  goal: "15 minutes",
+  percentageImprovement: 5, // 5% improvement
+};
+
+const reading = {
+  isDaily: true,
+  name: "reading",
+  cue: "When I see my book on my nightstand",
+  craving: "I want to gain knowledge and unwind before bed",
+  action: "I will read for 20 minutes",
+  reward: "I will expand my knowledge and have a peaceful bedtime routine",
+  goal: "20 minutes",
+  percentageImprovement: 5, // 5% improvement
+};
+
+const rowing = {
+  isDaily: true,
+  name: "rowing",
+  cue: "When I see the rowing machine in my home gym",
+  craving: "I want to improve my fitness and feel energized",
+  action: "I will row for 30 minutes",
+  reward: "I will have a sense of accomplishment and increased energy",
+  goal: "30 minutes",
+  percentageImprovement: 5, // 5% improvement
+};
+
+const painting = {
+  isDaily: true,
+  name: "painting",
+  cue: "When I see my art supplies on the table",
+  craving: "I want to express my creativity and relax through art",
+  action: "I will paint for 45 minutes",
+  reward: "I will have created something beautiful and feel relaxed",
+  goal: "45 minutes",
+  percentageImprovement: 5, // 5% improvement
+};
+
+const running = {
+  isDaily: true,
+  name: "running",
+  cue: "When I put on my running shoes",
+  craving: "I want to improve my cardiovascular health and boost my mood",
+  action: "I will go for a 20-minute run",
+  reward: "I will feel invigorated and accomplished",
+  goal: "20 minutes",
+  percentageImprovement: 5, // 5% improvement
+};
+
+const healthyEating = {
+  isDaily: true,
+  name: "healthyeating",
+  cue: "When I open the fridge for a snack",
+  craving: "I want to nourish my body and maintain a healthy weight",
+  action: "I will choose a healthy snack or meal option",
+  reward: "I will feel satisfied and maintain my health goals",
+  goal: "Make healthy choices throughout the day",
+  percentageImprovement: 5, // 5% improvement
+};
+
+const journaling = {
+  isDaily: true,
+  name: "journaling",
+  cue: "When I sit down at my desk in the evening",
+  craving: "I want to reflect on my day and reduce stress",
+  action: "I will journal for 15 minutes",
+  reward: "I will gain insights and feel emotionally balanced",
+  goal: "15 minutes",
+  percentageImprovement: 5, // 5% improvement
+};
+
+const languageLearning = {
+  isDaily: true,
+  name: "languagelearning",
+  cue: "When I have free time in the morning",
+  craving: "I want to become fluent in a new language",
+  action: "I will study and practice the language for 30 minutes",
+  reward: "I will feel accomplished and closer to my language goals",
+  goal: "30 minutes",
+  percentageImprovement: 5, // 5% improvement
+};
+
+const yoga = {
+  isDaily: true,
+  name: "yoga",
+  cue: "When I roll out my yoga mat",
+  craving: "I want to improve flexibility and reduce stress",
+  action: "I will do yoga for 20 minutes",
+  reward: "I will feel relaxed and rejuvenated",
+  goal: "20 minutes",
+  percentageImprovement: 5, // 5% improvement
+};
+
+const presets = [guitar, meditation, reading, rowing, painting];
