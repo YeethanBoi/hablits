645274c4957f924978dde77d3207c35f0ac8996d
YeethanBoi <yeethan007@gmail.com>
2023-10-16 09:39:43 +1030

    start on the habits page

--- src/habits/habits.js
+++ src/habits/habits.js
@@ -3,3 +3,21 @@
 import "../styles/main.css";
 const loader = document.getElementById("loader");
 loader.style.display = "none";
+
+document.querySelectorAll(".card").forEach((card) => {
+  card.addEventListener("click", (event) => {
+    event.currentTarget.classList.toggle("expanded");
+  });
+});
+
+document.querySelectorAll(".accordion").forEach((card) => {
+  card.addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.parentElement.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+});
